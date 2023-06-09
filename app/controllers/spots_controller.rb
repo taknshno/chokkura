2c2846468be5ccd20466b05ccabd36f38ab5a6ca
taknshno <126603921+taknshno@users.noreply.github.com>
2023-06-10 02:11:36 +0900

    [追加]Spotコントローラにindexアクションを定義した

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,2 +1,26 @@
 class SpotsController < ApplicationController
+  before_action :set_spot, only: %i[show edit update destroy]
+
+  def index
+    @spots = Spot.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def set_picture
+    @spot = Spot.find(params[:id])
+  end
+
+  def spot_params
+    params.require(:spot).permit(:name, :area, :address, :phone, :holiday, :sales_copy,
+                                  :detail_description, :simple_description, images: [])
+  end
 end
