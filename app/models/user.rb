903b9e2b18e29cd0355511f851758d79ec3aaa4b
taknshno <126603921+taknshno@users.noreply.github.com>
2023-06-15 17:27:13 +0900

    [追加]UserのShowページでお気に入りスポットを表示できるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :favorites, dependent: :destroy
-  has_many :spots, through: :favorites, source: :spot
+  has_many :favorite_spots, through: :favorites, source: :spot
 end
