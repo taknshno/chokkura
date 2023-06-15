7846b739bdb982f394f91abfcaf7afcb58e9d95b
taknshno <126603921+taknshno@users.noreply.github.com>
2023-06-15 17:45:40 +0900

    [追加]Userテーブルへlocationカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_15_070917) do
+ActiveRecord::Schema.define(version: 2023_06_15_082801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2023_06_15_070917) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
     t.string "name", null: false
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
