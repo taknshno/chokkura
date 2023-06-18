fb03bb42382deffc834e780a5518a72a300eba86
taknshno <126603921+taknshno@users.noreply.github.com>
2023-06-19 08:22:58 +0900

    [変更]Turbolinksを無効にした

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,5 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
@@ -16,10 +11,10 @@ import '../stylesheets/cards';
 import '../stylesheets/spot_show';
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
 
-$(document).on ("turbolinks:load", function(){
+$(function(){
   setTimeout(function () {
     $("#flash_msg").hide();
   }, 3000);
