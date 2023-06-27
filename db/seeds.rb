3114feb2a76ac3077c00c19a0a7cbce6096dfc02
taknshno <126603921+taknshno@users.noreply.github.com>
2023-06-28 02:33:13 +0900

    [追加]タグ用のシードデータを作成するコードを記述した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 Faker::Config.locale = :en
 
 ### User ###
+
 5.times do |n|
   first_name = Faker::Name.unique.first_name
   last_name = Faker::Name.unique.last_name
@@ -19,3 +20,37 @@ end
 
 puts "User Data ...ok"
 
+
+### Tag ###
+
+Tag.create!( name: "グルメ" )
+Tag.create!( name: "自然" )
+Tag.create!( name: "映えスポット" )
+Tag.create!( name: "お土産" )
+Tag.create!( name: "体験型" )
+Tag.create!( name: "歴史" )
+Tag.create!( name: "パワースポット" )
+
+puts "Tag Data ...ok"
+
+
+### Spot ###
+
+User.create!( name: "鹿島神宮",
+  #images: ,
+  area: ,
+  address:,
+  phone:,
+  holiday:,
+  sales_copy:,
+  detail_description:,
+  simple_description:
+ )
+# Tag.create!( name: "自然" )
+# Tag.create!( name: "映えスポット" )
+# Tag.create!( name: "お土産" )
+# Tag.create!( name: "体験型" )
+# Tag.create!( name: "歴史" )
+# Tag.create!( name: "パワースポット" )
+
+#puts "Spot Data ...ok"
