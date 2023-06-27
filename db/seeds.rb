58f55de2c91d8419ea5e3834237134ef79a21d13
taknshno <126603921+taknshno@users.noreply.github.com>
2023-06-28 00:43:34 +0900

    [追加]ユーザ用のシードデータを作成するコードを記述した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Faker::Config.locale = :en
+
+### User ###
+5.times do |n|
+  first_name = Faker::Name.unique.first_name
+  last_name = Faker::Name.unique.last_name
+  password = "password"
+  location = User.locations.keys.sample
+  admin = Faker::Boolean.boolean
+
+  User.create!( name: "#{first_name} #{last_name}",
+                email: "#{last_name.downcase}@example.com",
+                password: password,
+                password_confirmation: password,
+                location: location,
+                admin: admin
+               )
+end
+
+puts "User Data ...ok"
+
