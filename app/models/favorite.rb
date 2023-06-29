2ae1991b19d5d7c8a5843f60a125f280c2d1b0a4
taknshno <126603921+taknshno@users.noreply.github.com>
2023-06-29 09:04:12 +0900

    [追加]Favoriteモデルにuser_idとspot_idのペアの一意性を付与した

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :spot
+  validates :user_id, uniqueness: { scope: :spot_id }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_20_225424) do
+ActiveRecord::Schema.define(version: 2023_06_28_235840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_06_20_225424) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["spot_id"], name: "index_favorites_on_spot_id"
+    t.index ["user_id", "spot_id"], name: "index_favorites_on_user_id_and_spot_id", unique: true
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
